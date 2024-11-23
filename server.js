c76c79d53c92fb815c88bffed84ab137785d1cd6
AlexxsandroM <alexsandromacedo872@gmail.com>
2024-11-23 20:52:48 -0300

    Aula 3

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
-const posts = [
-  {
-    id: 1,
-    descricao: "Uma foto teste",
-    imagem: "https://placecats.com/millie/300/150"
-  },
-  {
-    id: 2,
-    descricao: "Gato fazendo yoga",
-    imagem: "https://placekitten.com/400/200"
-  },
-  {
-    id: 3,
-    descricao: "Paisagem com um gato",
-    imagem: "https://placekitten.com/600/300"
-  }
-];
+
 const app = express();
-app.use(express.json());
+routes(app);
 
-app.listen(3000,() =>{
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
   console.log("Servidor escutando...");
 });
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
 
-function buscarPostPorId(id) {
+
+
+
+
+
+
+
+
+/*function buscarPostPorId(id) {
   return posts.findIndex((post) => {
     return post.id === Number(id)
   }); 
@@ -37,4 +28,4 @@ function buscarPostPorId(id) {
 app.get("/posts/:id", (req, res) => {
   const index = buscarPostPorId(req.params.id);
   res.status(200).json(posts[index]);
-});
+});*/
---
+++ src/controllers/postsControlles.js
@@ -0,0 +1,10 @@
+import getTodosPosts from "../models/postModel.js";
+
+
+export async function listarPosts(req, res) {
+  // Obtém todos os posts usando a função getTodosPosts
+  const posts = await getTodosPosts();
+  // Envia os posts como resposta JSON com status 200 (OK)
+  res.status(200).json(posts);
+}
+
